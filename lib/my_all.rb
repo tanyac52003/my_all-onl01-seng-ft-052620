e3d36d3de477e59f2c328a3a6b828123e91598f7
Tanya Hodges <tanyac52003@gmail.com>
2020-05-28 16:27:35 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
-
+  index = 0 
+  while index < collection.length 
+  if ! yield(collection[index])
+    return false
+  end
+  index += 1 
+  end 
+true 
 end
